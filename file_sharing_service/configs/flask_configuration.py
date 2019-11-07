af02fb9af5f06f7d9ee00476bc15cb03240e2ed2
sandr463 <stanish2000@gmail.com>
2019-11-07 15:50:05 +0200

    Update flask configs with adding mail paramethers

--- file_sharing_service/configs/flask_configuration.py
+++ file_sharing_service/configs/flask_configuration.py
@@ -4,4 +4,12 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'testingforserve@gmail.com'
+app.config['MAIL_PASSWORD'] = 'StrongPassword98'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
 mail = Mail(app)
