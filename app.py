58d9b276a0114e1e9c68ca2091677b64ddb9348c
sandr463 <stanish2000@gmail.com>
2019-11-07 15:50:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from file_sharing_service.configs.flask_configuration import app
+from file_sharing_service.views.direct_downloading_view import DirectDownloadingView
+from file_sharing_service.views.email_sending_view import EmailSendingView
 
 
 if __name__ == '__main__':
