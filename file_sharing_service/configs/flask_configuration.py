a4422a2b37b7b512bb53b64d0ca8f284b119862e
sandr463 <stanish2000@gmail.com>
2019-11-18 13:05:31 +0200

    Delete configs for flask-mail

--- file_sharing_service/configs/flask_configuration.py
+++ file_sharing_service/configs/flask_configuration.py
@@ -1,15 +1,5 @@
 from flask import Flask
 from flask_restful import Api
-from flask_mail import Mail
 
 app = Flask(__name__)
 api = Api(app)
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'testingforserve@gmail.com'
-app.config['MAIL_PASSWORD'] = 'StrongPassword98'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-
-mail = Mail(app)
