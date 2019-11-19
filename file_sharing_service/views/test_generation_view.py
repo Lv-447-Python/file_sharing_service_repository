37a67ff35a6df9654234e60fe684469632a89e44
sandr463 <stanish2000@gmail.com>
2019-11-19 02:35:27 +0200

    Create test API for generating files

---
+++ file_sharing_service/views/test_generation_view.py
@@ -0,0 +1,17 @@
+from file_sharing_service.broker.event_handlers import emit_sending
+from file_sharing_service.configs.flask_configuration import api
+from flask_restful import Resource
+from flask import request
+
+
+class GenerateFileView(Resource):
+    def get(self):
+        response = {
+            'status': 200,
+            'msg': 'Success',
+            'filepath': '/home/andrew/file_sharing_service_repository/homes.csv'
+            }
+        return response
+
+
+api.add_resource(GenerateFileView, '/generate')
