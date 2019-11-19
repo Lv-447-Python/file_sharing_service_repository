fbdae233b8e73ea3396496fa9728570fd93aca8a
sandr463 <stanish2000@gmail.com>
2019-11-19 02:44:42 +0200

    Add endpoints to app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from file_sharing_service.configs.flask_configuration import app
 from file_sharing_service.views.direct_downloading_view import DirectDownloadingView
+from file_sharing_service.views.direct_downloading_view import DownloadingLinkView
 from file_sharing_service.views.email_sending_view import EmailSendingView
+from file_sharing_service.views.test_generation_view import GenerateFileView
 
 
 if __name__ == '__main__':
