296d00ab0517e701a645a7db1e230bb18bf53028
sandr463 <stanish2000@gmail.com>
2019-11-19 02:38:36 +0200

    Create view for downloading file and get downloading link

--- file_sharing_service/views/direct_downloading_view.py
+++ file_sharing_service/views/direct_downloading_view.py
@@ -2,12 +2,19 @@ from file_sharing_service.broker.event_handlers import emit_sending
 from file_sharing_service.configs import rabbit_configuration
 from file_sharing_service.configs.flask_configuration import api
 from flask_restful import Resource
+from flask import request, send_file
 
 
-class DirectDownloadingView(Resource):
-    def get(self, file):
+class DownloadingLinkView(Resource):
+    def get(self):
+        file_data = {
+            'user_id': request.args.get('user_id', type=int),
+            'file_id': request.args.get('file_id', type=int),
+            'filter_id': request.args.get('filter_id', type=int)
+        }
+        print('qweqw')
         emit_sending(
-            file,
+            file_data,
             queue_name=rabbit_configuration.file_queue_name,
             routing_key=rabbit_configuration.file_routing_key
         )
@@ -15,4 +22,11 @@ class DirectDownloadingView(Resource):
         return 200
 
 
-api.add_resource(DirectDownloadingView, '/file-download')
+class DirectDownloadingView(Resource):
+    def get(self):
+        filepath = request.args.get('filepath', type=str)
+        return send_file(filepath, as_attachment=True)
+
+
+api.add_resource(DirectDownloadingView, '/download')
+api.add_resource(DownloadingLinkView, '/download-link')
