4dafc01bb06b1f0714468e601a853a0895943531
sandr463 <stanish2000@gmail.com>
2019-11-19 02:43:53 +0200

    Create worker to generate downloading links

---
+++ file_sharing_service/broker/generate_link_worker.py
@@ -0,0 +1,22 @@
+import requests
+from file_sharing_service.configs.flask_configuration import api
+
+
+def get_filepath(user_id, filter_id, file_id):
+    response = requests.get(f'http://localhost:5000/generate?file_id={file_id}&filter_id={filter_id}&user_id={user_id}')
+
+    json_response = response.json()
+    filepath = json_response['filepath']
+
+    return filepath
+
+
+def generate_downloading_link(filepath):
+    print(f'127.0.0.1:5000/download?filepath={filepath}')
+    return f'127.0.0.1/download/{filepath}'
+
+
+def send_file(file_data):
+    filepath = get_filepath(file_data['user_id'], file_data['filter_id'], file_data['file_id'])
+    downloading_link = generate_downloading_link(filepath)
+    return downloading_link
