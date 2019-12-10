8577aff192b0be67bb5d2632079831d79cfbc155
sandr463 <stanish2000@gmail.com>
2019-12-10 02:13:30 +0200

    Create file compression

--- file_sharing_service/broker/send_email_worker.py
+++ file_sharing_service/broker/send_email_worker.py
@@ -1,12 +1,23 @@
 """Script that sends email with file"""
 import smtplib
 import ssl
+import os
+import zipfile
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from file_sharing_service.configs import smtp_configuration
-from .generate_link_worker import get_filepath
+from file_sharing_service import APP
+
+
+def make_archive(filepath):
+    zip_filename = f'{filepath}.zip'
+    zip_file = zipfile.ZipFile(zip_filename, 'w')
+    zip_file.write(filepath, compress_type=zipfile.ZIP_DEFLATED)
+    zip_file.close()
+
+    return zip_filename
 
 
 def send_email(file_data):
@@ -20,20 +31,28 @@ def send_email(file_data):
     message.attach(MIMEText(body, 'plain'))
     receiver_email = file_data['email']
 
-    filename = get_filepath(file_data['user_id'],
-                            file_data['filter_id'],
-                            file_data['file_id'])
+    filename = file_data['filename']
+
+    file_sharing_dir = os.path.dirname(APP.root_path)
+    uploads_dir = os.path.join(file_sharing_dir, APP.config['UPLOAD_FOLDER'])
+    filepath = uploads_dir + filename
+
+    file_size = os.path.getsize(filepath)
+
+    if file_size > 1024:
+        print('archive')
+        filename = make_archive(filepath)
 
     try:
-        with open(filename, 'rb') as attachment:
+        with open(filepath, 'rb') as attachment:
             part = MIMEBase('application', 'octet-stream')
             part.set_payload(attachment.read())
+
     except FileNotFoundError:
         print('File not found')
         return None
 
     encoders.encode_base64(part)
-
     part.add_header('Content-Disposition', f'attachment; filename={filename}')
 
     message.attach(part)
@@ -43,3 +62,7 @@ def send_email(file_data):
     with smtplib.SMTP_SSL('smtp.gmail.com', port=smtp_configuration.PORT, context=context) as server:
         server.login(smtp_configuration.SENDER_EMAIL, smtp_configuration.SENDER_PASSWORD)
         server.sendmail(smtp_configuration.SENDER_EMAIL, receiver_email, text)
+
+    if filename.split('.')[-1] == 'zip':
+        os.remove(filename)
+        print('file has been removed')
