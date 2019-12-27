f1589921f447f8047f431e77583bcd7ace25d915
ssastanishevskyi <stanish2000@gmail.com>
2019-12-27 03:19:38 +0200

    Fix bugs with database changes

--- file_sharing_service/broker/delete_timer_worker.py
+++ file_sharing_service/broker/delete_timer_worker.py
@@ -15,12 +15,12 @@ def deletion_timer(file_data):
 
     """
     creation_time = datetime.datetime.strptime(file_data['file_datetime'], '%Y-%m-%dT%H:%M:%S.%f')
-    deletion_time = creation_time + datetime.timedelta(seconds=100)
-    file_id = file_data['id']
+    deletion_time = creation_time + datetime.timedelta(hours=24)
+    file_name = file_data['file_name']
 
     try:
         time.sleep((deletion_time - datetime.datetime.now()).total_seconds())
-        response = requests.delete(f'http://127.0.0.1:5000/download/{file_id}')
+        response = requests.delete(f'http://127.0.0.1:5000/download/{file_name}')
         LOGGER.info('API call for deletion was sent')
         LOGGER.info(f'Response: {response.json()}')
     except ValueError:
